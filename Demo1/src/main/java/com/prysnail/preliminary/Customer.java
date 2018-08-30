27f380be59f3b399ebd529d45a9f76f863abd805
prysnail <prysnail@outlook.com>
2018-08-30 10:33:45 +0800

    demo1:pry:2 规范命名

--- Demo1/src/main/java/com/prysnail/preliminary/Customer.java
+++ Demo1/src/main/java/com/prysnail/preliminary/Customer.java
@@ -22,40 +22,40 @@ public class Customer {
     //需要重构方法
     public String statement(){
         double totalAmount = 0;
-        int frequentRenterPoints = 0;
+        int renterPoints = 0;
         Enumeration rentals = retals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()){
             double thisAmount = 0;
-            Rental each = (Rental) rentals.nextElement();
+            Rental myRental = (Rental) rentals.nextElement();
 
-            switch (each.getMovie().getPriceCode()){
+            switch (myRental.getMovie().getPriceCode()){
                 case Movie.REGULAR:
                     thisAmount += 2;
-                    if (each.getDaysRented() >2)
-                        thisAmount += (each.getDaysRented() -2) * 1.5;
+                    if (myRental.getDaysRented() >2)
+                        thisAmount += (myRental.getDaysRented() -2) * 1.5;
                     break;
                 case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
+                    thisAmount += myRental.getDaysRented() * 3;
                     break;
                 case Movie.CHILDRENS:
                     thisAmount += 1.5;
-                    if (each.getDaysRented() >3)
-                        thisAmount += (each.getDaysRented() -3) * 1.5;
+                    if (myRental.getDaysRented() >3)
+                        thisAmount += (myRental.getDaysRented() -3) * 1.5;
                     break;
             }
 
-            frequentRenterPoints++;
+            renterPoints++;
 
-            if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE &&
-                    each.getDaysRented() > 1) frequentRenterPoints++;
+            if (myRental.getMovie().getPriceCode() == Movie.NEW_RELEASE &&
+                    myRental.getDaysRented() > 1) renterPoints++;
 
-            result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf(thisAmount) +"\n";
+            result += "\t" + myRental.getMovie().getTitle()+ "\t" + String.valueOf(thisAmount) +"\n";
             totalAmount += thisAmount;
         }
 
         result += "Amount owed is "+ String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints)+
+        result += "You earned " + String.valueOf(renterPoints)+
                 " frequent renter points";
         return result;
     }
