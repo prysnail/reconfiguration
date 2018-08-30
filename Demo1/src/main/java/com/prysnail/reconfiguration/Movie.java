9b257b6e2435a4e6834fe51d41e7551e02ca6c4f
prysnail <prysnail@outlook.com>
2018-08-30 23:58:26 +0800

    Demo1:pry:7 使用继承重构

--- Demo1/src/main/java/com/prysnail/reconfiguration/Movie.java
+++ Demo1/src/main/java/com/prysnail/reconfiguration/Movie.java
@@ -7,10 +7,11 @@ public class Movie {
 
     private String title;
     private int priceCode;
+    private Price price;
 
-    public Movie(String title, int priceCode){
+    public Movie(String title, int priceCode) throws IllegalAccessException {
         title = title;
-        priceCode = priceCode;
+        setPriceCode(priceCode);
     }
 
     public String getTitle() {
@@ -22,37 +23,27 @@ public class Movie {
     }
 
     public int getPriceCode() {
-        return priceCode;
+        return price.getPriceCode();
     }
 
-    public void setPriceCode(int priceCode) {
-        this.priceCode = priceCode;
+    public void setPriceCode(int arg) throws IllegalAccessException {
+        switch (arg){
+            case REGULAR:
+                price = new RegularPrice();
+            case CHILDRENS:
+                price = new ChildrenPrice();
+            case NEW_RELEASE:
+                price = new NewReleasePrice();
+            default:
+                throw new IllegalAccessException("Incorrect Price Code");
+        }
     }
     
     public double getCharge(int daysRented){
-        double result = 0;
-        switch (getPriceCode()){
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented >2)
-                    result += (daysRented -2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented >3)
-                    result += (daysRented -3) * 1.5;
-                break;
-        }
-        return result;
+        return price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented){
-        if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
-            return 2;
-        else
-            return 1;
+        return price.getFrequentRenterPoints(daysRented);
     }
 }
---
+++ Demo1/src/main/java/com/prysnail/reconfiguration/Price.java
@@ -0,0 +1,11 @@
+package com.prysnail.reconfiguration;
+
+public abstract class Price {
+    abstract int getPriceCode();
+
+    abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented){
+        return 1;
+    }
+}
