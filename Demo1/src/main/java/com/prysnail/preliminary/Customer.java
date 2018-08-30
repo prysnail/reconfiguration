d2abba470d84bd295b204cfc3a9646924a48a3f9
prysnail <prysnail@outlook.com>
2018-08-30 10:12:03 +0800

    demo1:pry:1 删除无意义的注释

--- Demo1/src/main/java/com/prysnail/preliminary/Customer.java
+++ Demo1/src/main/java/com/prysnail/preliminary/Customer.java
@@ -29,7 +29,6 @@ public class Customer {
             double thisAmount = 0;
             Rental each = (Rental) rentals.nextElement();
 
-            //determine amounts for each line
             switch (each.getMovie().getPriceCode()){
                 case Movie.REGULAR:
                     thisAmount += 2;
@@ -46,17 +45,15 @@ public class Customer {
                     break;
             }
 
-            //add frequent renter points
             frequentRenterPoints++;
-            //add bonus for a two day ew release rental
+
             if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE &&
                     each.getDaysRented() > 1) frequentRenterPoints++;
 
-            //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + String.valueOf(thisAmount) +"\n";
             totalAmount += thisAmount;
         }
-        //add footer lines
+
         result += "Amount owed is "+ String.valueOf(totalAmount) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints)+
                 " frequent renter points";
